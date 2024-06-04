345ad863caf3d182e4b623e346e4fba55abc7664
WTCHAI <103483111+WTCHAI@users.noreply.github.com>
2024-06-04 19:38:10 +0700

    Styling Examevent Usability & debugging sessionstorage getting

--- src/components/home/examEvent.tsx
+++ src/components/home/examEvent.tsx
@@ -2,9 +2,13 @@
 
 import React, { useState , useEffect} from 'react';
 
+import Link from 'next/link';
+
 import { FeedBlogDetail } from '@/interface/feedBlogs/feedBlogDetails';
 import { FeedStatus } from '@/interface/feedBlogs/feedstatus';
 import { onGetExamEvent } from '@/firebase/assets/home/examEvent';
+
+import { FaArrowCircleRight } from "react-icons/fa";
 
 import { Skeleton } from 'antd';
 
@@ -50,6 +54,7 @@ export default function ExamEvent() {
     sessionStorage.setItem('examEvent', JSON.stringify(response.payload.content))
     // console.log(response.payload.content)
     const tempFeeds: FeedBlogDetail[] = response.payload.content
+    // console.log(tempFeeds)
     onSetCurrentFeeds({
         currentFeeds : tempFeeds,
         status: response.status,
@@ -64,7 +69,7 @@ export default function ExamEvent() {
       isLoading : true
     })
      if (sessionStorage.getItem('examEvent')) {
-          const tempFeeds : FeedBlogDetail[] = (JSON.parse(sessionStorage.getItem('feeds') as string)) 
+          const tempFeeds : FeedBlogDetail[] = (JSON.parse(sessionStorage.getItem('examEvent') as string)) 
 
           onSetCurrentFeeds({
               ...Feeds,
@@ -78,16 +83,13 @@ export default function ExamEvent() {
       }   
   },[])
   return (
-    <section className="bg-slate-600 py-[20vh]">
+    <section className="bg-slate-600 pt-[15vh] pb-[10vh]">
       <div className="flex flex-col px-4 items-center justify-center">
         <h2 className="text-3xl font-semibold text-slate-50 mb-8 text-center">Explore Upcoming Events</h2>
       </div>
-      <ul className="flex flex-row gap-8 overflow-x-scroll px-[5vw]">
-
-          {/* Event cards would be dynamically generated here */}
-
+      <ul className="flex flex-row items-center  gap-8 overflow-x-scroll  mx-[5vw] pb-[10vh] px-[5vw]">
           {
-            Feeds.currentFeeds?.map((feed,index) => {
+            Feeds.currentFeeds?.map((feed) => {
               //date translation
               const startDate = new Date(feed.startDate);
               const endDate = new Date(feed.endDate);
@@ -98,14 +100,14 @@ export default function ExamEvent() {
               return (
                 <li
                   key={feed.eventId}
-                  className="bg-gray-100 px-[10vw] rounded-lg shadow-md w-[30vw]"
+                  className="bg-gray-100 px-[3vw] rounded-lg shadow-md min-w-fit py-[2vh]"
                 >
                   <h3 className="text-xl font-semibold text-gray-800">{feed.eventTitle}</h3>
                   <p className="text-gray-600 mt-2">{feed.eventDescription}</p>
                   <p className="text-red-500 text-xs font-normal mb-2">
                     {translateDay(TransDateS[0])}, {TransDateS[1]} {TransDateS[2]} {TransDateE[3]}
                   </p>
-                  <a href="#" className="text-primaryBlue mt-4 inline-block">Learn More</a>
+                  <Link href="#" className="text-primaryBlue mt-4 inline-block">Learn More</Link>
                 </li>  
               )
           })}
@@ -114,6 +116,16 @@ export default function ExamEvent() {
               <Skeleton active />
             </li>
           }
+          
+          <li 
+            className='ml-[2vw]'
+          >
+            <Link href="/explore">
+              <FaArrowCircleRight className='text-5xl text-slate-50'/>
+            </Link>
+            
+          </li>
+          
 
           {/* Repeat above block for more events */}
       </ul>
